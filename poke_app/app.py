7d34fde54bdbfb64cac0e82f9ca28b24ce40ca5f
joaoribeirosl <joaoribeiroslira@gmail.com>
2025-04-09 17:45:26 -0300

    feat: first unit test

--- poke_app/app.py
+++ poke_app/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
-def hello_world():
-    return {'message': 'como q escrev'}
+def pokemon():
+    return {'message': 'IPlayPokemonGoEveryDay'}
