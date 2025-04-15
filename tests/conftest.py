69fca2e58579a40b235b24de1f724ce678fb0882
joaoribeirosl <joaoribeiroslira@gmail.com>
2025-04-15 11:54:11 -0300

    feat: create async db test to create user

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import factory
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 from sqlalchemy.pool import StaticPool
 
@@ -18,6 +21,26 @@ class UserFactory(factory.Factory):
     username = factory.Sequence(lambda x: f'test{x}')
     email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
     password = factory.LazyAttribute(lambda obj: f'{obj.username}!1')
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
 
 
 @pytest.fixture
---
+++ tests/test_db.py
@@ -0,0 +1,28 @@
+from dataclasses import asdict
+
+import pytest
+from sqlalchemy import select
+
+from poke_app.models import User
+
+
+@pytest.mark.asyncio
+async def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='jojo', password='seiaa', email='test@test'
+        )
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'jojo'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'jojo',
+        'password': 'seiaa',
+        'email': 'test@test',
+        'created_at': time,
+        'updated_at': time,
+        'pokemon': [],
+    }
