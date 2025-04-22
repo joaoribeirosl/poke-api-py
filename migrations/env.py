90aa91c4a4e0ac1bd442a2cda1f3a4863a06d15c
joaoribeirosl <joaoribeiroslira@gmail.com>
2025-04-22 19:00:18 -0300

    feat: create docker compose, dockerfile and entrypoint + code cleanup

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,6 @@
 import asyncio
 
+
 from logging.config import fileConfig
 
 from sqlalchemy.ext.asyncio import async_engine_from_config
@@ -18,24 +19,8 @@ if config.config_file_name is not None:
 
 target_metadata = table_registry.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
--- poke_app/schemas.py
+++ poke_app/schemas.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
 
 from poke_app.utils import validate_type
