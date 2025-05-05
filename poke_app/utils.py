b36f83c3ad4306d4a2d7842317ea9f84e8ecf066
joaoribeirosl <joaoribeiroslira@gmail.com>
2025-05-05 08:05:52 -0300

    feat: add method to fetch pokemon data from poke api

--- poke_app/utils.py
+++ poke_app/utils.py
@@ -1,3 +1,11 @@
+from time import sleep
+
+import requests
+
+POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon'
+POKEDEX = 151
+
+
 VALID_TYPES = {
     'normal',
     'fire',
@@ -43,3 +51,11 @@ def validate_type(poke_type: str) -> str:
     if poke_type not in VALID_TYPES:
         raise ValueError(f"Invalid Pokemon type: '{poke_type}'")
     return poke_type
+
+
+def fetch_pokemon_data(poke_id):
+    sleep(0.3)
+    url = f'{POKE_API_URL}/{poke_id}'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
