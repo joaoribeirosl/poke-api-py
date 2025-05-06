7fb6de109b3ea2ad600d76b78675f9b5290a8f54
joaoribeirosl <joaoribeiroslira@gmail.com>
2025-05-06 09:26:06 -0300

    refactor: clean up old migrations and create users, pokemon and types tables + seeds

--- migrations/versions/7bbe7e498c06_create_users_table.py
+++ migrations/versions/7bbe7e498c06_create_users_table.py
@@ -1,8 +1,8 @@
 """create users table
 
-Revision ID: 6caa0112eba4
+Revision ID: 7bbe7e498c06
 Revises: 
-Create Date: 2025-05-04 21:41:45.478599
+Create Date: 2025-05-06 12:00:16.766885
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '6caa0112eba4'
+revision: str = '7bbe7e498c06'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
